99361456fc6b677aa1ec06a270c38991bfd45e89
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-01 01:17:13 +0100

    update

--- frontend/main.js
+++ frontend/main.js
@@ -3,9 +3,14 @@ import { Room, RoomEvent } from "./modules/livekit-client.esm.js"
 const videoGrid = document.querySelector('.video-grid')
 
 const handleTrackSubsribed = function (track, publication, participant){
-    //const videoTag = document.createElement('video')
+    const videoTag = track.attach(videoTag)
+    videoTag.style.height = '300px'
+    videoTag.style.width = '200px'
+    videoTag.style.backgroundColor = 'black'
+    videoTag.autoplay = true
+    videoTag.playsinline = true
     console.log('Track event triggered')
-    videoGrid.appendChild(track.attach())
+    videoGrid.appendChild(videoTag)
 }
 
 const getToken = async function() {
